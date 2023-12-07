6be91d07c7db737a34c3d4c395cba6b334444af8
drumath2237 <2237k.drumath@gmail.com>
2023-12-08 02:26:37 +0900

    :tada: いい感じの簡単なデモができた

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import "./style.scss";
-import { Engine, MeshBuilder, Scene, Vector3 } from "@babylonjs/core";
+import {
+  Engine,
+  IWebXRRawCameraAccessOptions,
+  MeshBuilder,
+  Scene,
+  StandardMaterial,
+  Vector3,
+  WebXRFeatureName,
+  WebXRRawCameraAccess,
+} from "@babylonjs/core";
 
 const main = async () => {
   const renderCanvas = document.getElementById(
@@ -9,14 +18,50 @@ const main = async () => {
     return;
   }
 
+  // ========sceneの構築========
+
   const engine = new Engine(renderCanvas, true);
   const scene = new Scene(engine);
 
   scene.createDefaultCameraOrLight(true, true, true);
-  scene.createDefaultEnvironment();
 
-  const box = MeshBuilder.CreateBox("box", { size: 0.1 });
-  box.position = new Vector3(0, 0.05, 0);
+  const box = MeshBuilder.CreateBox("box", { size: 0.15 });
+  box.position = new Vector3(0, 0, 0.3);
+
+  const material = new StandardMaterial("material", scene);
+  box.material = material;
+
+  // ========WebXR機能の実装========
+
+  const xr = await scene.createDefaultXRExperienceAsync({
+    uiOptions: {
+      sessionMode: "immersive-ar",
+      referenceSpaceType: "unbounded",
+    },
+  });
+  const sessionManager = xr.baseExperience.sessionManager;
+  const featureManager = xr.baseExperience.featuresManager;
+
+  // WebXRRawCameraAccess Featureを初期化・取得
+  const rawCameraAccess = featureManager.enableFeature(
+    WebXRFeatureName.RAW_CAMERA_ACCESS,
+    "latest",
+    {} as IWebXRRawCameraAccessOptions
+  ) as WebXRRawCameraAccess;
+
+  rawCameraAccess.onTexturesUpdatedObservable.add(() => {
+    console.log("texture update!");
+    if (rawCameraAccess.texturesData.length !== 0) {
+      material.diffuseTexture = rawCameraAccess.texturesData[0];
+    }
+  });
+
+  sessionManager.onXRFrameObservable.add(() => {
+    const intrinsics = rawCameraAccess.cameraIntrinsics;
+    if (intrinsics.length !== 0) {
+      console.log(rawCameraAccess.cameraIntrinsics);
+    }
+  });
 
   window.addEventListener("resize", () => engine.resize());
   engine.runRenderLoop(() => scene.render());
