ef6fe60f6d143340db933aa39577141667fbb4bf
drumath <2237k.drumath@gmail.com>
2023-12-08 03:04:26 +0900

    :green_heart: setup ci

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig(({ mode }) => {
+  if (mode === "production") {
+    return {
+      base: "/babylon-webxr-raw-camera-access-sandbox/",
+    };
+  }
+
   return {
     plugins: [basicSsl()],
   };
